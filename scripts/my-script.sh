540dfea5836fd541b36cc5c1a06bdac5a6009311
k23165720 <k23165720@kcl.ac.uk>
2026-03-06 13:29:38 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,10 +1,9 @@
- #!/usr/bin/env bash
- # make empty directories in your git repo
- mkdir -p analysis docs data
- 
- # add a README.md to each directory
- # the scripts directory already exists
- for my_directory in scripts analysis docs data;do
-   touch ${my_directiry}/README.ms
-   echo "# ${my_directoiry}" >> ${my_directory}/README.ms
- done
+#!/usr/bin/env bash
+# make empty directories in your git repo
+mkdir -p analysis docs data
+# add a README.md to each directory
+# the scripts directory already exists
+for my_directory in scripts analysis docs data;do
+ touch ${my_directory}/README.md
+ echo "# ${my_directory}" >> ${my_directory}/README.md
+done
